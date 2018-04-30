0e0bb784395915385c652453e842d1d1af1ab40e
Stig Røsnes <androidstigrr@gmail.com>
2018-04-30 10:45:12 +0200

    first commit

---
+++ config.cpp
@@ -0,0 +1,13 @@
+#include "basicdefines_A3.hpp"
+class DefaultEventhandlers;
+
+#include "cfgPatches.hpp"
+
+class UniformSlotInfo;
+
+class cfgWeapons
+{
+	class InventoryItem_Base_F;
+	class ItemCore;
+	#include "cfgVests.hpp"
+};
